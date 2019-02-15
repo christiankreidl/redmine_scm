24cab3bca4df2bac638a0d9230931131e600beb6
Will Simpson <wsimpson@email.arizona.edu>
2019-02-15 12:18:58 -0700

    Specify version of rails in database migration.

--- db/migrate/001_add_repositories_created_with_scm.rb
+++ db/migrate/001_add_repositories_created_with_scm.rb
@@ -1,4 +1,4 @@
-class AddRepositoriesCreatedWithScm < ActiveRecord::Migration
+class AddRepositoriesCreatedWithScm < ActiveRecord::Migration[5.0]
 
     def self.up
         add_column :repositories, :created_with_scm, :boolean, :default => false, :null => false
